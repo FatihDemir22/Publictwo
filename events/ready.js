32ff032f41db6c2185f77801dd74744912f2e4c3
Glitch (165-komut) <none>
2022-05-17 19:44:21 +0000

    🏦🍤 Checkpoint ./events/ready.js:44839670/9

--- events/ready.js
+++ events/ready.js
@@ -7,16 +7,16 @@ var prefix = ayarlar.prefix;
 
 module.exports = client => {
 var oyun = [
-        "Fatih ❤ S P E A W E R A",
+        "Fatih ❤️ S P E A W E R A",
         "Komutum .yardım",
-        "Fatih ❤ S P E A W E R A",
+        "Fatih ❤️ S P E A W E R A",
     ];
 
     setInterval(function() {
 
         var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
 
-        client.user.setActivity(oyun[random], "Fatih Demir" );
+        client.user.setActivity(oyun[random], "Fatih Demir ❤️" );
         }, 2 * 2500);
     
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
